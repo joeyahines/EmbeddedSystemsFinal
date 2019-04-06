f2c92443ee154819f523350fa63ee55152c7cf06
Joey Hines <joey@ahines.net>
2019-04-05 20:13:55 -0500

    Added motor code and line follower code

--- main.c
+++ main.c
@@ -1,6 +1,5 @@
 /*
  * TODO
- * + Add timer motor code
  * + Finish line follower code
  * + Add collision handler
  * + Add path finding logic (need to build the maze)
@@ -10,7 +9,19 @@
 #include "msp.h"
 #include "CortexM.h"
 
+//Possible Motor Values
+enum motor{Right = BIT6, Left = BIT7};
+
+int16_t right_motor_speed;
+int16_t left_motor_speed;
+
+//Line Data
+int16_t line_data;
+
+//TimerA0 Period
 uint16_t PERIOD;
+
+//Clock Globals
 uint32_t ClockFrequency = 3000000; // cycles/second
 
 int32_t Prewait = 0;                   // loops between BSP_Clock_InitFastest() called and PCM idle (expect 0)
@@ -26,6 +37,8 @@ void(*bump_event)(uint8_t);
 void(*line_follower_event)(uint8_t);
 void set_left_motor_duty(uint16_t);
 void set_right_motor_duty(uint16_t);
+void set_motor_power(int8_t power_level, enum motor m);
+uint8_t line_follower_read(void);
 
 void delay(unsigned long ulCount){
   __asm (  "pdloop:  subs    r0, #1\n"
@@ -56,15 +69,6 @@ void SysTick_Init(void){
 
 }
 
-void SysTick_Init_Int(uint32_t period){
-  SysTick->CTRL = 0x00000000;
-  SysTick->LOAD = period -1;           // maximum reload value
-  SysTick->VAL = 0;
-  SCB->SHP[11] =4<<5;
-  SysTick->CTRL = 0x00000007;           // enable SysTick with interrupts
-}
-
-
 void SysTick_Wait(uint32_t delay){
   SysTick->LOAD = (delay - 1);// count down to zero
   SysTick->VAL = 0;          // any write to CVR clears it and COUNTFLAG in CSR
@@ -78,6 +82,21 @@ void SysTick_Wait10ms(uint32_t delay){
   for(i=0; i<delay; i++){
     SysTick_Wait(480000);  // wait 10ms (assumes 48 MHz clock)
   }
+}
+
+void SysTick_Delay1us(uint32_t delay){
+  uint32_t i;
+  for(i=0; i<delay/2; i++){
+    SysTick_Wait(2);  // wait 1ms (assumes 48 MHz clock)
+  }
+}
+
+void Clock_Delay1us(uint32_t n) {
+    n = (382*n)/100;
+
+    while(n) {
+        n--;
+    }
 }
 
 // delay function
@@ -209,6 +228,7 @@ void Clock_Init48MHz(void){
 //  SubsystemFrequency = 12000000;
 }
 
+
 /*************************************
  *  Launchpad init
  ************************************/
@@ -239,7 +259,6 @@ void Motor_Init(void){
     //P3->DS |= 0xC0;
 
     P1->OUT &= ~0xC0;
-    P2->OUT &= ~0xC0;   // off
     P3->OUT &= ~0xC0;   // low current sleep mode
 
 
@@ -249,47 +268,71 @@ void PWM_init(uint16_t period, uint16_t duty1, uint16_t duty2) {
     P2->DIR |= BIT6 | BIT7;
     P2->SEL0 |= BIT6 | BIT7;
     P2->SEL1 &= BIT6 | BIT7;
+
     TIMER_A0->CCTL[0] = 0x0080;
     TIMER_A0->CCR[0] = period;
+
     TIMER_A0->EX0 = 0x0000;
+
     TIMER_A0->CCTL[3] = 0x0040;
     TIMER_A0->CCR[3] = duty1;
+
     TIMER_A0->CCTL[4] = 0x0040;
     TIMER_A0->CCR[4] = duty2;
+
     TIMER_A0->CTL = 0x02F0;
+
     PERIOD = period;
 }
 
 void set_right_motor_power(int8_t power_level) {
+    set_motor_power(power_level, Right);
+}
+
+void set_left_motor_power(int8_t power_level) {
+    set_motor_power(power_level, Left);
+}
+
+void set_motor_power(int8_t power_level, enum motor m) {
     uint16_t duty;
+
 
     if (power_level != 0) {
         if (power_level < 0) {
-            P1->OUT |= BIT6;
+            P1->OUT |= m;
             power_level = power_level*-1;
         }
         else {
-            P1-> OUT & ~BIT6;
+            P1-> OUT & ~m;
+        }
+
+        if (power_level > 100) {
+            power_level = 100;
         }
 
         duty = ((uint16_t)power_level * PERIOD)/100;
-
-        set_right_motor_duty(duty);
-        P3->OUT |= BIT6;
+        P3->OUT |= m;
     }
     else {
-        set_right_motor_duty(0);
-        P3->OUT |= BIT6;
+        duty = 0;
+        P3->OUT &= ~m;
+    }
+
+    if (m == Right) {
+        set_right_motor_duty(duty);
+    }
+    else {
+        set_left_motor_duty(duty);
     }
 }
 
 
 void set_left_motor_duty(uint16_t duty) {
-    TIMER_A0->CCR[3] = duty;
+    TIMER_A0->CCR[4] = duty;
 }
 
 void set_right_motor_duty(uint16_t duty) {
-    TIMER_A0->CCR[4] = duty;
+    TIMER_A0->CCR[3] = duty;
 }
 
 
@@ -344,14 +387,6 @@ void BumpInt_Init(void(*task)(uint8_t)){
     NVIC->ISER[1] = 0x00000040;
 }
 
-void line_follower_init(void(*task)(uint8_t)) {
-    P7->SEL0 &= ~0xFF;
-    P7->SEL1 &= ~0xFF;
-    P7->DIR &= ~0xFF;
-    P7->REN |= 0xFF;
-    P7->OUT |= 0xFF;
-}
-
 // Read current state of 6 switches
 // Returns a 6-bit positive logic result (0 to 63)
 // bit 5 Bump5
@@ -373,9 +408,75 @@ uint8_t Bump_Read(void){
     return (bump0 | bump1 | bump2 | bump3 | bump4 | bump5); // replace this line
 }
 
-uint8_t line_follower_read(void) {
-    return P7->IN;
+void follow_line(int16_t distance_from_line){
+    unsigned int base_power = 50;
+    int right_motor_power;
+    int left_motor_power;
+
+    if (distance_from_line < 0) {
+        distance_from_line *= -1;
+        right_motor_power = base_power;
+        left_motor_power = ((((((uint16_t)distance_from_line << 2)/(uint16_t)332) * (base_power)) + (base_power << 2)) >> 2);
+    }
+    else {
+        right_motor_power = ((((((uint16_t)distance_from_line << 2)/(uint16_t)332) * (base_power)) + (base_power << 2)) >> 2);
+        left_motor_power = base_power;
+    }
+    left_motor_speed = left_motor_power;
+    right_motor_speed = right_motor_power;
+    set_right_motor_power(right_motor_power);
+    set_left_motor_power(left_motor_power);
 }
+
+
+int16_t get_distance_from_line(uint8_t line_value) {
+    int16_t distances[] = {-332, -237, -142, -47, 47, 237, 332};
+    int16_t sum = 0;
+    int16_t divisor = 0;
+    uint8_t weight;
+    uint8_t i;
+
+    for (i=0; i < 8; i++) {
+        weight = ((line_value & (BIT0 << i) ) >> i);
+        sum += distances[i]*weight;
+        divisor += weight;
+    }
+
+    return sum/divisor;
+}
+
+uint8_t line_follower_read(void) {
+    uint8_t result;
+    //Turn on IR LEDS
+    P5->OUT |= BIT3;
+
+    //Set P7 High
+    P7->DIR = 0xFF;
+    P7->OUT = 0xFF;
+
+    //Wait 10us
+    Clock_Delay1us(10);
+    //SysTick_Delay1us(15);
+
+    //Make P7 input
+    P7->DIR = 0x00;
+
+    //Allow RC circuit to decay
+    //SysTick_Delay1us(1400);
+    Clock_Delay1us(2000);
+
+    //read data
+    result = P7->IN;
+
+    //Turn off IR LED
+    P5->OUT &= ~BIT3;
+
+    //wait for everything to turn off
+    //SysTick_Delay1us(15);
+    Clock_Delay1us(10);
+    return result;
+}
+
 
 // we do not care about critical section/race conditions
 // triggered on touch, falling edge
@@ -386,16 +487,6 @@ void PORT4_IRQHandler(void){
         (*bump_event)(status);
     }
 }
-
-void PORT7_IRQHandler(void) {
-
-}
-
-void SysTick_Handler(void)
-{
-    P1->OUT ^= 0x01;
-}
-
 /***************************
  * main.c
  ***************************/
@@ -405,39 +496,46 @@ void HandleCollision(uint8_t bumpSensor){
    CollisionFlag = 1;
 }
 
-
-
-#define FORWARD 0b11100111;
-#define RIGHT 0b11001111
-#define MORE_RIGHT 0b10011111
-#define SHARP_RIGHT 0b00111111
-#define LEFT 0b11110011
-#define MORE_LEFT 0b11111001
-#define SHARP_LEFT 0b11111100
-#define LEFT_TURN 0b00001111
-#define RIGHT_TURN 0b1110000
-#define OOPS 0b11111111
-#define TREASURE 0b0011001100
-void handle_line_follower_update(uint8_t line_follower){
-
-}
-
-
-
-
 int main(void){  // test of interrupt-driven bump interface
+
+  uint8_t line_follower_data;
+  int16_t distance_from_line;
   Clock_Init48MHz();   // 48 MHz clock; 12 MHz Timer A clock
   CollisionFlag = 0;
-        // activate Lab 5 - PWM
+
   SysTick_Init();
   Motor_Init();
-  line_follower_init(0);
   LaunchPad_Init();
-  uint8_t value = 0;
+  PWM_init(8000, 0, 0);
+  EnableInterrupts();
 
-  PWM_init(1000, 0, 0);
+  P7->SEL0 &= ~0xFF;
+  P7->SEL1 &= ~0xFF;
+  P7->DIR &= ~0xFF;
+  P7->DS |= 0xFF;
+  P7->REN &= ~0xFF;
+  P7->OUT |= 0xFF;
 
+  P5->SEL0 &= ~BIT3;
+  P5->SEL1 &= ~BIT3;
+  P5->DIR |= BIT3;
+  P5->OUT &= ~BIT3;
+  P5->DS |= BIT3;
+
+  Pause();
+  //set_right_motor_power(50);
+  //set_left_motor_power(50);
 
   while(1){
+      line_follower_data = line_follower_read();
+
+      if (line_follower_data) {
+          distance_from_line = get_distance_from_line(line_follower_data);
+          follow_line(distance_from_line);
+      }
+      else {
+          set_right_motor_power(0);
+          set_left_motor_power(0);
+      }
   }
 }
